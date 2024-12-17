10f270405bbf936950e7d7713e25679381734c04
Alex <alex.polonski@gmail.com>
2024-12-17 16:26:12 +0100

    (feat) - Switch from InMemoryCustomerRepository to MongoDBCustomerRepository. Update DB URL in all config files. Requires refactoring.

--- src/infrastructure/Server.ts
+++ src/infrastructure/Server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response, NextFunction } from "express";
-import { CustomerService } from "../application/CustomerService";
-import { InMemoryCustomerRepository } from "./persistence/repositories/InMemoryCustomerRepository";
-import { CustomerController } from "../interfaces/controllers/CustomerController";
+import express, {NextFunction, Request, Response} from "express";
+import {CustomerService} from "../application/CustomerService";
+import {CustomerController} from "../interfaces/controllers/CustomerController";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import dotenv from "dotenv";
+import {MongoDBCustomerRepository} from "./persistence/repositories/MongoDBCustomerRepository";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -15,22 +15,30 @@ app.use(express.json());
 
 // Swagger Configuration
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Motorbike Shop API",
-      version: "1.0.0",
-      description: "API for managing customers of an online motorbike shop",
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Motorbike Shop API",
+            version: "1.0.0",
+            description: "API for managing customers of an online motorbike shop",
+        },
     },
-  },
-  apis: ["./src/interfaces/controllers/*.ts"], // Path to the API docs
+    apis: ["./src/interfaces/controllers/*.ts"], // Path to the API docs
 };
 
 const specs = swaggerJsdoc(options);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Configuring Services and Drivers
-const customerRepository = new InMemoryCustomerRepository();
+// const customerRepository = new InMemoryCustomerRepository();
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const dbName = process.env.DB_NAME || "motorbike-shop-api";
+const collectionName = "customers";
+const customerRepository = new MongoDBCustomerRepository(
+    uri,
+    dbName,
+    collectionName
+);
 
 app.locals.customerRepository = customerRepository;
 const customerService = new CustomerService(app.locals.customerRepository);
@@ -39,51 +47,51 @@ const customerController = new CustomerController(customerService);
 
 // Definition of routes
 app.post("/customers", (req: Request, res: Response) =>
-  customerController.createCustomer(req, res)
+    customerController.createCustomer(req, res)
 );
 app.get("/customers", (req: Request, res: Response) =>
-  customerController.listCustomers(req, res)
+    customerController.listCustomers(req, res)
 );
 app.get("/customers/sortByCredit", (req: Request, res: Response) =>
-  customerController.sortCustomersByCredit(req, res)
+    customerController.sortCustomersByCredit(req, res)
 );
 app.get("/customers/:id", (req: Request, res: Response) =>
-  customerController.getCustomerById(req, res)
+    customerController.getCustomerById(req, res)
 );
 app.put("/customers/:id", (req: Request, res: Response) =>
-  customerController.updateCustomer(req, res)
+    customerController.updateCustomer(req, res)
 );
 // TODO add Patch method
 app.delete("/customers/:id", (req: Request, res: Response) =>
-  customerController.deleteCustomer(req, res)
+    customerController.deleteCustomer(req, res)
 );
 app.post("/customers/credit", (req: Request, res: Response) =>
-  customerController.addCredit(req, res)
+    customerController.addCredit(req, res)
 );
 
 // Error handling
 /* istanbul ignore next */
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  res.format({
-    'application/json': () => next(),
-    'default': () => next(),  // Continue to the next middleware
-  });
-  console.error(err.stack);
-  const errorMessage = "Something broke!";
-  res.setHeader('Content-Type', 'application/json');
-  res.status(500).json({
-    error: errorMessage + ' ' + (err as Error).message,
-  });
+    res.format({
+        'application/json': () => next(),
+        'default': () => next(),  // Continue to the next middleware
+    });
+    console.error(err.stack);
+    const errorMessage = "Something broke!";
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).json({
+        error: errorMessage + ' ' + (err as Error).message,
+    });
 });
 
 // Start server
 /* istanbul ignore next */
 if (process.env.NODE_ENV !== "test") {
-  const PORT = process.env.PORT || 3000; // Use PORT from .env or fallback to 3000
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(
-      `API documentation available at http://localhost:${PORT}/api-docs`
-    );
-  });
+    const PORT = process.env.PORT || 3000; // Use PORT from .env or fallback to 3000
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(
+            `API documentation available at http://localhost:${PORT}/api-docs`
+        );
+    });
 }
